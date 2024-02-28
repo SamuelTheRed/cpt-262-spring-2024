1b6fa57e68dba4db3d3c155ce339bcf9b4f3526f
SamuelTheRed <samuelshelley02@gmail.com>
2024-02-27 23:21:03 -0500

    Assignment 6 Initialization

---
+++ shelley_assignment6/public/scripts/insertcourse.js
@@ -0,0 +1,350 @@
+var CourseBox = React.createClass({
+  handleCourseSubmit: function (course) {
+    $.ajax({
+      url: "/course",
+      dataType: "json",
+      type: "POST",
+      data: course,
+      success: function (data) {
+        this.setState({ data: data });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function () {
+    return (
+      <div className="CourseBox">
+        <h1>Courses</h1>
+        <Courseform onCourseSubmit={this.handleCourseSubmit} />
+      </div>
+    );
+  },
+});
+
+var Courseform = React.createClass({
+  getInitialState: function () {
+    return {
+      coursesemester: "",
+      courseyear: "",
+      facultyid: "",
+      data: []
+    };
+  },
+  handleOptionChange: function (e) {
+    this.setState({
+      selectedOption: e.target.value
+    });
+  },
+  loadFac: function () {
+    $.ajax({
+      url: '/getfac',
+      dataType: 'json',
+      cache: false,
+      success: function (data) {
+        this.setState({ data: data });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  componentDidMount: function () {
+    this.loadFac();
+
+  },
+  handleSubmit: function (e) {
+    e.preventDefault();
+
+    var courseid = this.state.courseid;
+    var courseprefix = this.state.courseprefix.trim();
+    var coursenumber = this.state.coursenumber.trim();
+    var coursesection = this.state.coursesection.trim();
+    var coursesemester = this.state.coursesemester.trim();
+    var courseyear = this.state.courseyear.trim();
+    var facultyid = facnum.value;
+
+    if (!coursesemester || !courseyear) {
+      console.log("Field Missing");
+      return;
+    }
+
+    this.props.onCourseSubmit({
+      courseid: courseid,
+      courseprefix: courseprefix,
+      coursenumber: coursenumber,
+      coursesection: coursesection,
+      coursesemester: coursesemester,
+      courseyear: courseyear,
+      facultyid: facultyid
+    });
+  },
+
+  validateEmail: function (value) {
+    var re =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(value);
+  },
+  validateDollars: function (value) {
+    var regex = /^\$?[0-9]+(\.[0-9][0-9])?$/;
+    return regex.test(value);
+  },
+  commonValidate: function () {
+    return true;
+  },
+  setValue: function (field, event) {
+    var object = {};
+    object[field] = event.target.value;
+    this.setState(object);
+  },
+  render: function () {
+    return (
+      <form className="CourseForm" onSubmit={this.handleSubmit}>
+        <h2>Insert Course</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th>Course ID</th>
+              <td>
+                <TextInput
+                  value={this.state.courseid}
+                  uniqueName="courseid"
+                  textArea={false}
+                  required={true}
+                  minCharacters={0}
+                  validate={this.commonValidate}
+                  onChange={this.setValue.bind(this, "courseid")}
+                  errorMessage="Course ID is invalid"
+                  emptyMessage="Course ID is required"
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>Course Prefix</th>
+              <td>
+                <TextInput
+                  value={this.state.courseprefix}
+                  uniqueName="courseprefix"
+                  textArea={false}
+                  required={true}
+                  minCharacters={3}
+                  validate={this.commonValidate}
+                  onChange={this.setValue.bind(this, "courseprefix")}
+                  errorMessage="Course Name is invalid"
+                  emptyMessage="Course Name is Required"
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>Course Number</th>
+              <td>
+                <TextInput
+                  value={this.state.coursenumber}
+                  uniqueName="coursenumber"
+                  textArea={false}
+                  required={true}
+                  minCharacters={2}
+                  validate={this.commonValidate}
+                  onChange={this.setValue.bind(this, "coursenumber")}
+                  errorMessage="Invalid Course Number"
+                  emptyMessage="Course Number is Required"
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>Course Section</th>
+              <td>
+                <TextInput
+                  value={this.state.coursesection}
+                  uniqueName="coursesection"
+                  textArea={false}
+                  required={false}
+                  minCharacters={3}
+                  validate={this.commonValidate}
+                  onChange={this.setValue.bind(this, "coursesection")}
+                  errorMessage="Course Section is invalid"
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>Course Semester</th>
+              <td>
+                <TextInput
+                  value={this.state.coursesemester}
+                  uniqueName="coursesemester"
+                  textArea={false}
+                  required={true}
+                  minCharacters={3}
+                  validate={this.commonValidate}
+                  onChange={this.setValue.bind(this, "coursesemester")}
+                  errorMessage="Course Section is invalid"
+                  emptyMessage="Course Section is required"
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>Course Year</th>
+              <td>
+                <TextInput
+                  value={this.state.courseyear}
+                  uniqueName="courseyear"
+                  textArea={false}
+                  required={true}
+                  minCharacters={3}
+                  validate={this.commonValidate}
+                  onChange={this.setValue.bind(this, "courseyear")}
+                  errorMessage="Course Year is invalid"
+                  emptyMessage="Course Year is required"
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>
+                Course Faculty
+              </th>
+              <td>
+                <SelectList data={this.state.data} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <input type="submit" value="Insert Course"/>
+      </form>
+    );
+  },
+});
+
+var InputError = React.createClass({
+  getInitialState: function () {
+    return {
+      message: "Input is invalid",
+    };
+  },
+  render: function () {
+    var errorClass = classNames(this.props.className, {
+      error_container: true,
+      visible: this.props.visible,
+      invisible: !this.props.visible,
+    });
+
+    return <td> {this.props.errorMessage} </td>;
+  },
+});
+
+var TextInput = React.createClass({
+  getInitialState: function () {
+    return {
+      isEmpty: true,
+      value: null,
+      valid: false,
+      errorMessage: "",
+      errorVisible: false,
+    };
+  },
+
+  handleChange: function (event) {
+    this.validation(event.target.value);
+
+    if (this.props.onChange) {
+      this.props.onChange(event);
+    }
+  },
+
+  validation: function (value, valid) {
+    if (typeof valid === "undefined") {
+      valid = true;
+    }
+
+    var message = "";
+    var errorVisible = false;
+
+    if (!valid) {
+      message = this.props.errorMessage;
+      valid = false;
+      errorVisible = true;
+    } else if (this.props.required && jQuery.isEmptyObject(value)) {
+      message = this.props.emptyMessage;
+      valid = false;
+      errorVisible = true;
+    } else if (value.length < this.props.minCharacters) {
+      message = this.props.errorMessage;
+      valid = false;
+      errorVisible = true;
+    }
+
+    this.setState({
+      value: value,
+      isEmpty: jQuery.isEmptyObject(value),
+      valid: valid,
+      errorMessage: message,
+      errorVisible: errorVisible,
+    });
+  },
+
+  handleBlur: function (event) {
+    var valid = this.props.validate(event.target.value);
+    this.validation(event.target.value, valid);
+  },
+  render: function () {
+    if (this.props.textArea) {
+      return (
+        <div className={this.props.uniqueName}>
+          <textarea
+            placeholder={this.props.text}
+            className={"input input-" + this.props.uniqueName}
+            onChange={this.handleChange}
+            onBlur={this.handleBlur}
+            value={this.props.value}
+          />
+
+          <InputError
+            visible={this.state.errorVisible}
+            errorMessage={this.state.errorMessage}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div className={this.props.uniqueName}>
+          <input
+            name={this.props.uniqueName}
+            id={this.props.uniqueName}
+            placeholder={this.props.text}
+            className={"input input-" + this.props.uniqueName}
+            onChange={this.handleChange}
+            onBlur={this.handleBlur}
+            value={this.props.value}
+          />
+
+          <InputError
+            visible={this.state.errorVisible}
+            errorMessage={this.state.errorMessage}
+          />
+        </div>
+      );
+    }
+  },
+});
+
+var SelectList = React.createClass({
+  render: function () {
+    var optionNodes = this.props.data.map(function (facID) {
+      return (
+        <option
+          key={facID.facultyid}
+          value={facID.facultyid}
+        >
+          {facID.facultyfirstname} {facID.facultylastname}
+        </option>
+      );
+    });
+    return (
+      <select name="facnum" id="facnum">
+        {optionNodes}
+      </select>
+    );
+  }
+});
+
+ReactDOM.render(<CourseBox />, document.getElementById("content"));
---
+++ shelley_assignment6/server.js
@@ -0,0 +1,106 @@
+"use strict";
+var fs = require("fs");
+var path = require("path");
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+const mysql = require("mysql2");
+
+const con = mysql.createConnection({
+  host: "istwebclass.org",
+  user: "sshell18_cpt262remote",
+  password: "f3b6b5eb-7238-4f07-aaa6-d49848d2ec7c",
+  database: "sshell18_epic262_epic",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.set("port", process.env.PORT || 3000);
+
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.post("/faculty/", function (req, res) {
+  var ftitle = req.body.facultytitle;
+  var ffirstname = req.body.facultyfirstname;
+  var flastname = req.body.facultylastname;
+  var femail = req.body.facultyemail;
+
+  var sqlins =
+    "INSERT INTO epicfaculty (facultytitle, facultyfirstname, facultylastname, " +
+    " facultyemail) VALUES (?, ?, ?, ?)";
+  var inserts = [ftitle, ffirstname, flastname, femail];
+
+  var sql = mysql.format(sqlins, inserts);
+
+  con.execute(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+    res.redirect("insertfaculty.html");
+    res.end();
+  });
+});
+
+app.post("/course/", function (req, res) {
+  var cid = req.body.courseid;
+  var csemester = req.body.coursesemester;
+  var cyear = req.body.courseyear;
+  var facultyid = req.body.facultyid;
+
+  var checkid = "SELECT * from epiccourses where courseid = " + cid;
+
+  var sql = mysql.format(checkid);
+
+  con.query(sql, function (err, data) {
+    if (err) throw err;
+
+    if (data.length > 0) {
+      console.log(data[0]["courseid"]);
+
+      var sqlins =
+        "INSERT INTO epicschedule (schedulesemester, scheduleyear, facultyID, " +
+        " courseID) VALUES (?, ?, ?, ?)";
+      var inserts = [csemester, cyear, facultyid, cid];
+
+      var sql = mysql.format(sqlins, inserts);
+
+      console.log(sql);
+
+      con.execute(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+        res.redirect("insertcourse.html");
+        res.end();
+      });
+    } else {
+      console.log("Course ID Does Not Exist");
+    }
+  });
+});
+
+app.get("/getfac/", function (req, res) {
+  var sqlsel = "select * from epicfaculty";
+  var sql = mysql.format(sqlsel);
+
+  con.query(sql, function (err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+    res.send(JSON.stringify(data));
+  });
+});
+
+app.listen(app.get("port"), function () {
+  console.log("Server started: http://localhost:" + app.get("port") + "/");
+});
