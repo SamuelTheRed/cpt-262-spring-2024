6a3ec24898bcc11719ea63d19eb844f7dbc24e3b
SamuelTheRed <samuelshelley02@gmail.com>
2024-03-15 10:39:16 -0400

    Search Products Page is Functional

--- shelley_milestone3/public/scripts/searchproducts.js
+++ shelley_milestone3/public/scripts/searchproducts.js
@@ -1,175 +1,174 @@
 var ProductBox = React.createClass({
-    getInitialState: function () {
-      return { data: [] };
-    },
-    loadProductsFromServer: function () {
-      console.log(productid.value);
-      $.ajax({
-        url: "/getproduct",
-        data: {
-            'productid': productid.value,
-            'productname': productname.value,
-            'productprice': productprice.value,
-            'productquantity': productquantity.value,
-        },
-        dataType: "json",
-        cache: false,
-        success: function (data) {
-          this.setState({ data: data });
-        }.bind(this),
-        error: function (xhr, status, err) {
-          console.error(this.props.url, status, err.toString());
-        }.bind(this),
-      });
-    },
-    componentDidMount: function () {
-      this.loadProductsFromServer();
-    },
-  
-    render: function () {
+  getInitialState: function () {
+    return { data: [] };
+  },
+  loadProductsFromServer: function () {
+    console.log(productid.value);
+    $.ajax({
+      url: "/getproduct",
+      data: {
+        productid: productid.value,
+        productname: productname.value,
+        productprice: productprice.value,
+        productquantity: productquantity.value,
+      },
+      dataType: "json",
+      cache: false,
+      success: function (data) {
+        this.setState({ data: data });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this),
+    });
+  },
+  componentDidMount: function () {
+    this.loadProductsFromServer();
+  },
+
+  render: function () {
+    return (
+      <div>
+        <h1>Products</h1>
+        <Productform onProductSubmit={this.loadProductsFromServer} />
+        <br />
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <ProductList data={this.state.data} />
+        </table>
+      </div>
+    );
+  },
+});
+
+var Productform = React.createClass({
+  getInitialState: function () {
+    return {
+      productid: "",
+      productname: "",
+      productprice: "",
+      productquantity: "",
+    };
+  },
+  handleSubmit: function (e) {
+    e.preventDefault();
+
+    var productid = this.state.productid.trim();
+    var productname = this.state.productname.trim();
+    var productprice = this.state.productprice.trim();
+    var productquantity = this.state.productquantity.trim();
+
+    this.props.onProductSubmit({
+      productid: productid,
+      productname: productname,
+      productprice: productprice,
+      productquantity: productquantity,
+    });
+  },
+  handleChange: function (event) {
+    this.setState({
+      [event.target.id]: event.target.value,
+    });
+  },
+  render: function () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Search Through Products</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th>Product ID</th>
+              <td>
+                <input
+                  type="text"
+                  name="productid"
+                  id="productid"
+                  value={this.state.productid}
+                  onChange={this.handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>Product Name</th>
+              <td>
+                <input
+                  name="productname"
+                  id="productname"
+                  value={this.state.productname}
+                  onChange={this.handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>Product Price</th>
+              <td>
+                <input
+                  name="productprice"
+                  id="productprice"
+                  value={this.state.productprice}
+                  onChange={this.handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <th>Product Quantity</th>
+              <td>
+                <input
+                  name="productquantity"
+                  id="productquantity"
+                  value={this.state.productquantity}
+                  onChange={this.handleChange}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <input type="submit" value="Search Product" />
+      </form>
+    );
+  },
+});
+var ProductList = React.createClass({
+  render: function () {
+    var productNodes = this.props.data.map(function (product) {
+      //map the data to individual
       return (
-        <div>
-          <h1>Products</h1>
-          <Productform onProductSubmit={this.loadProductsFromServer} />
-          <br />
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Prefix</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Description</th>
-              </tr>
-            </thead>
-            <ProductList data={this.state.data} />
-          </table>
-        </div>
+        <Product
+          key={product.dbproduct_id}
+          pdcid={product.dbproduct_id}
+          pdcname={product.dbproduct_name}
+          pdcprice={product.dbproduct_price}
+          pdcquantity={product.dbproduct_quantity}
+          pdcdescription={product.dbproduct_description}
+        ></Product>
       );
-    },
-  });
-  
-  var Productform = React.createClass({
-    getInitialState: function () {
-      return {
-        productid: "",
-        productname: "",
-        productprice: "",
-        productquantity: "",
-      };
-    },
-    handleSubmit: function (e) {
-      e.preventDefault();
-  
-      var productid = this.state.productid.trim();
-      var productname = this.state.productname.trim();
-      var productprice = this.state.productprice.trim();
-      var productquantity = this.state.productquantity.trim();
-  
-      this.props.onProductSubmit({
-        productid: productid,
-        productname: productname,
-        productprice: productprice,
-        productquantity: productquantity,
-      });
-    },
-    handleChange: function (event) {
-      this.setState({
-        [event.target.id]: event.target.value,
-      });
-    },
-    render: function () {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          <h2>Search Through Products</h2>
-          <table>
-            <tbody>
-              <tr>
-                <th>Product ID</th>
-                <td>
-                  <input
-                    type="text"
-                    name="productid"
-                    id="productid"
-                    value={this.state.productid}
-                    onChange={this.handleChange}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <th>Product Prefix</th>
-                <td>
-                  <input
-                    name="productname"
-                    id="productname"
-                    value={this.state.productname}
-                    onChange={this.handleChange}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <th>Product Price</th>
-                <td>
-                  <input
-                    name="productprice"
-                    id="productprice"
-                    value={this.state.productprice}
-                    onChange={this.handleChange}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <th>Product Quantity</th>
-                <td>
-                  <input
-                    name="productquantity"
-                    id="productquantity"
-                    value={this.state.productquantity}
-                    onChange={this.handleChange}
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <input type="submit" value="Search Product" />
-        </form>
-      );
-    },
-  });
-  var ProductList = React.createClass({
-    render: function () {
-      var productNodes = this.props.data.map(function (product) {
-        //map the data to individual
-        return (
-          <Product
-            key={product.productid}
-            pdcid={product.productid}
-            pdcname={product.productname}
-            pdcprice={product.productprice}
-            pdcquantity={product.productquantity}
-            pdcdescription={product.productdescription}
-          ></Product>
-        );
-      });
-  
-      //print all the nodes in the list
-      return <tbody>{productNodes}</tbody>;
-    },
-  });
-  
-  var Product = React.createClass({
-    render: function () {
-      return (
-        <tr>
-          <td>{this.props.pdcid}</td>
-          <td>{this.props.pdcname}</td>
-          <td>{this.props.pdcprice}</td>
-          <td>{this.props.pdcquantity}</td>
-          <td>{this.props.pdcdescription}</td>
-        </tr>
-      );
-    },
-  });
-  
-  ReactDOM.render(<ProductBox />, document.getElementById("content"));
-  
+    });
+
+    //print all the nodes in the list
+    return <tbody>{productNodes}</tbody>;
+  },
+});
+
+var Product = React.createClass({
+  render: function () {
+    return (
+      <tr>
+        <td>{this.props.pdcid}</td>
+        <td>{this.props.pdcname}</td>
+        <td>${this.props.pdcprice}</td>
+        <td>{this.props.pdcquantity}</td>
+        <td>{this.props.pdcdescription}</td>
+      </tr>
+    );
+  },
+});
+
+ReactDOM.render(<ProductBox />, document.getElementById("content"));
--- shelley_milestone3/server.js
+++ shelley_milestone3/server.js
@@ -1,24 +1,24 @@
-'use strict';
-var fs = require('fs');
-var path = require('path');
-var express = require('express');
-var bodyParser = require('body-parser');
+"use strict";
+var fs = require("fs");
+var path = require("path");
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
-var bcrypt = require('bcrypt');
+var bcrypt = require("bcrypt");
 
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-// const con = mysql.createConnection({
-//     host: "istwebclass.org",
-//     user: "sshell18_cpt262remote",
-//     password: "f3b6b5eb-7238-4f07-aaa6-d49848d2ec7c",
-//     database: "sshell18_Spring2024",
-// });
+const con = mysql.createConnection({
+  host: "istwebclass.org",
+  user: "sshell18_cpt262remote",
+  password: "f3b6b5eb-7238-4f07-aaa6-d49848d2ec7c",
+  database: "sshell18_cpt262_fall2024_final",
+});
 
-// con.connect(function (err) {
-//     if (err) throw err;
-//     console.log("Connected!!");
-// });
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!!");
+});
 
 app.set("port", process.env.PORT || 3000);
 
@@ -30,6 +30,38 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-app.listen(app.get("port"), function () {
-    console.log("Server started: http://localhost:" + app.get("port") + "/");
+app.get("/getproduct/", function (req, res) {
+  var pid = req.query.productid;
+  var pname = req.query.productname;
+  var pprice = req.query.productprice;
+  var pquantity = req.query.productquantity;
+
+  var sqlsel =
+    "SELECT * " +
+    "FROM Products " +
+    "WHERE dbproduct_id LIKE ? AND dbproduct_name LIKE ? AND dbproduct_price LIKE ? " +
+    "AND dbproduct_quantity LIKE ? ";
+
+  var inserts = [
+    "%" + pid + "%",
+    "%" + pname + "%",
+    "%" + pprice + "%",
+    "%" + pquantity + "%",
+  ];
+
+  var sql = mysql.format(sqlsel, inserts);
+
+  console.log(sql);
+
+  con.query(sql, function (err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.send(JSON.stringify(data));
   });
+});
+
+app.listen(app.get("port"), function () {
+  console.log("Server started: http://localhost:" + app.get("port") + "/");
+});
