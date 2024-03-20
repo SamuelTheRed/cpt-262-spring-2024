ceb81f301f3679b0a825eedcf9ac1c6be7bb2c3e
SamuelTheRed <samuelshelley02@gmail.com>
2024-03-20 12:32:23 -0400

    Insert Reservations Functionality Complete

--- shelley_milestone3/public/scripts/insertreservations.js
+++ shelley_milestone3/public/scripts/insertreservations.js
@@ -1,258 +1,381 @@
 var ReservationBox = React.createClass({
-    handleReservationSubmit: function (reservation) {
-      $.ajax({
-        url: "/reservation",
-        dataType: "json",
-        type: "POST",
-        data: reservation,
-        success: function (data) {
-          this.setState({ data: data });
-        }.bind(this),
-        error: function (xhr, status, err) {
-          console.error(this.props.url, status, err.toString());
-        }.bind(this)
-      });
-    },
-    render: function () {
-      return (
-        <div className="ReservationBox">
-          <div className="page_title"><h1>Reservations</h1></div>
-          <Reservationform onReservationSubmit={this.handleReservationSubmit} />
+  handleReservationSubmit: function (reservation) {
+    $.ajax({
+      url: "/reservation",
+      dataType: "json",
+      type: "POST",
+      data: reservation,
+      success: function (data) {
+        this.setState({ data: data });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this),
+    });
+  },
+  render: function () {
+    return (
+      <div className="ReservationBox">
+        <div className="page_title">
+          <h1>Reservations</h1>
         </div>
-      );
-    },
-  });
-  
-  var Reservationform = React.createClass({
-    getInitialState: function () {
-      return {
-        reservationdatetime: "",
-      };
-    },
-    handleSubmit: function (e) {
-      e.preventDefault();
-  
-      var reservationdatetime = this.state.reservationdatetime.trim();
-  
-      if (!reservationdatetime) {
-        console.log("Field Missing");
-        return;
-      }
-  
-      this.props.onReservationSubmit({
-        reservationdatetime: reservationdatetime,
-      });
-    },
-    validateEmail: function (value) {
-      var re =
-        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(value);
-    },
-    validateDollars: function (value) {
-      var regex = /^\$?[0-9]+(\.[0-9][0-9])?$/;
-      return regex.test(value);
-    },
-    commonValidate: function () {
-      return true;
-    },
-    setValue: function (field, event) {
-      var object = {};
-      object[field] = event.target.value;
-      this.setState(object);
-    },
-    render: function () {
-      return (
-        <form className="ReservationForm" onSubmit={this.handleSubmit}>
-          <h2>Insert Reservation</h2>
-          <table>
+        <Reservationform onReservationSubmit={this.handleReservationSubmit} />
+      </div>
+    );
+  },
+});
+
+var Reservationform = React.createClass({
+  getInitialState: function () {
+    return {
+      reservationdate: "",
+      reservationtime: "",
+      plrdata: [],
+      usrdata: [],
+    };
+  },
+  handleOptionChange: function (e) {
+    this.setState({
+      selectedOption: e.target.value,
+    });
+  },
+  loadPlrData: function () {
+    $.ajax({
+      url: "/getplrdata",
+      dataType: "json",
+      cache: false,
+      success: function (plrdata) {
+        this.setState({ plrdata: plrdata });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this),
+    });
+  },
+  loadUsrData: function () {
+    $.ajax({
+      url: "/getusrdata",
+      dataType: "json",
+      cache: false,
+      success: function (usrdata) {
+        this.setState({ usrdata: usrdata });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this),
+    });
+  },
+  componentDidMount: function () {
+    this.loadPlrData();
+    this.loadUsrData();
+  },
+  handleSubmit: function (e) {
+    e.preventDefault();
+
+    var reservationdate = this.state.reservationdate.trim();
+    var reservationtime = this.state.reservationtime.trim();
+    var playerid = plrnum.value;
+    var userid = usrnum.value;
+
+    if (!reservationdate || !reservationtime || !playerid) {
+      console.log("Field Missing");
+      return;
+    }
+
+    this.props.onReservationSubmit({
+      reservationdate: reservationdate,
+      reservationtime: reservationtime,
+      playerid: playerid,
+      userid: userid,
+    });
+  },
+  validateEmail: function (value) {
+    var re =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(value);
+  },
+  validateDollars: function (value) {
+    var regex = /^\$?[0-9]+(\.[0-9][0-9])?$/;
+    return regex.test(value);
+  },
+  commonValidate: function () {
+    return true;
+  },
+  setValue: function (field, event) {
+    var object = {};
+    object[field] = event.target.value;
+    this.setState(object);
+  },
+  render: function () {
+    return (
+      <form className="form_area" onSubmit={this.handleSubmit}>
+        <h2>Insert Reservation</h2>
+        <div className="table_area">
+          <table className="form_table">
             <tbody>
               <tr>
-                <th>Reservation Date Time</th>
+                <th>Reservation Date</th>
                 <td>
                   <DateInput
-                    value={this.state.reservationdatetime}
-                    uniqueName="reservationdatetime"
+                    value={this.state.reservationdate}
+                    uniqueName="reservationdate"
                     textArea={false}
                     required={true}
                     minCharacters={3}
                     validate={this.commonValidate}
-                    onChange={this.setValue.bind(this, "reservationdatetime")}
-                    errorMessage="Reservation Name is invalid"
-                    emptyMessage="Reservation Name is Required"
+                    onChange={this.setValue.bind(this, "reservationdate")}
+                    errorMessage="Reservation Date is invalid"
+                    emptyMessage="Reservation Date is Required"
                   />
                 </td>
               </tr>
-            <tr>
-              <th>Order Customer</th>
-              <td>
-                <PlayerList />
-              </td>
-            </tr>
-            <tr>
-              <th>Order Employee</th>
-              <td>
-                <UserList />
-              </td>
-            </tr>
+              <tr>
+                <th>Reservation Time</th>
+                <td>
+                  <TimeInput
+                    value={this.state.reservationtime}
+                    uniqueName="reservationtime"
+                    textArea={false}
+                    required={true}
+                    minCharacters={3}
+                    validate={this.commonValidate}
+                    onChange={this.setValue.bind(this, "reservationtime")}
+                    errorMessage="Reservation Time is invalid"
+                    emptyMessage="Reservation Time is Required"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th>Reservation Player</th>
+                <td>
+                  <PlayerList data={this.state.plrdata} />
+                </td>
+              </tr>
+              <tr>
+                <th>Reservation User</th>
+                <td>
+                  <UserList data={this.state.usrdata} />
+                </td>
+              </tr>
             </tbody>
           </table>
-          <input type="submit" value="Insert Reservation"/>
-        </form>
-      );
-    },
-  });
-  
-  var InputError = React.createClass({
-    getInitialState: function () {
-      return {
-        message: "Input is invalid",
-      };
-    },
-    render: function () {
-      var errorClass = classNames(this.props.className, {
-        error_container: true,
-        visible: this.props.visible,
-        invisible: !this.props.visible,
-      });
-  
-      return <td> {this.props.errorMessage} </td>;
-    },
-  });
-  
-  var DateInput = React.createClass({
-    getInitialState: function () {
-      return {
-        isEmpty: true,
-        value: null,
-        valid: false,
-        errorMessage: "",
-        errorVisible: false,
-      };
-    },
-  
-    handleChange: function (event) {
-      this.validation(event.target.value);
-  
-      if (this.props.onChange) {
-        this.props.onChange(event);
-      }
-    },
-  
-    validation: function (value, valid) {
-      if (typeof valid === "undefined") {
-        valid = true;
-      }
-  
-      var message = "";
-      var errorVisible = false;
-  
-      if (!valid) {
-        message = this.props.errorMessage;
-        valid = false;
-        errorVisible = true;
-      } else if (this.props.required && jQuery.isEmptyObject(value)) {
-        message = this.props.emptyMessage;
-        valid = false;
-        errorVisible = true;
-      } else if (value.length < this.props.minCharacters) {
-        message = this.props.errorMessage;
-        valid = false;
-        errorVisible = true;
-      }
-  
-      this.setState({
-        value: value,
-        isEmpty: jQuery.isEmptyObject(value),
-        valid: valid,
-        errorMessage: message,
-        errorVisible: errorVisible,
-      });
-    },
-  
-    handleBlur: function (event) {
-      var valid = this.props.validate(event.target.value);
-      this.validation(event.target.value, valid);
-    },
-    render: function () {
-      if (this.props.textArea) {
-        return (
-          <div className={this.props.uniqueName}>
-            <textarea
-              placeholder={this.props.text}
-              className={"input input-" + this.props.uniqueName}
-              onChange={this.handleChange}
-              onBlur={this.handleBlur}
-              value={this.props.value}
-            />
-  
-            <InputError
-              visible={this.state.errorVisible}
-              errorMessage={this.state.errorMessage}
-            />
-          </div>
-        );
-      } else {
-        return (
-          <div className={this.props.uniqueName}>
-            <input
-              name={this.props.uniqueName}
-              type="time"
-              id={this.props.uniqueName}
-              placeholder={this.props.text}
-              className={"input input-" + this.props.uniqueName}
-              onChange={this.handleChange}
-              onBlur={this.handleBlur}
-              value={this.props.value}
-              step="480"
-              min="10:00"
-              max="18:00"
-            />
-  
-            <InputError
-              visible={this.state.errorVisible}
-              errorMessage={this.state.errorMessage}
-            />
-          </div>
-        );
-      }
-    },
-  });
+          <input
+            type="submit"
+            className="form_submit"
+            value="Insert Reservation"
+          />
+        </div>
+      </form>
+    );
+  },
+});
 
-  var PlayerList = React.createClass({
-    render: function () {
+var InputError = React.createClass({
+  getInitialState: function () {
+    return {
+      message: "Input is invalid",
+    };
+  },
+  render: function () {
+    var errorClass = classNames(this.props.className, {
+      error_container: true,
+      visible: this.props.visible,
+      invisible: !this.props.visible,
+    });
+
+    return <td> {this.props.errorMessage} </td>;
+  },
+});
+
+var DateInput = React.createClass({
+  getInitialState: function () {
+    return {
+      isEmpty: true,
+      value: null,
+      valid: false,
+      errorMessage: "",
+      errorVisible: false,
+    };
+  },
+
+  handleChange: function (event) {
+    this.validation(event.target.value);
+
+    if (this.props.onChange) {
+      this.props.onChange(event);
+    }
+  },
+
+  validation: function (value, valid) {
+    if (typeof valid === "undefined") {
+      valid = true;
+    }
+
+    var message = "";
+    var errorVisible = false;
+
+    if (!valid) {
+      message = this.props.errorMessage;
+      valid = false;
+      errorVisible = true;
+    } else if (this.props.required && jQuery.isEmptyObject(value)) {
+      message = this.props.emptyMessage;
+      valid = false;
+      errorVisible = true;
+    } else if (value.length < this.props.minCharacters) {
+      message = this.props.errorMessage;
+      valid = false;
+      errorVisible = true;
+    }
+
+    this.setState({
+      value: value,
+      isEmpty: jQuery.isEmptyObject(value),
+      valid: valid,
+      errorMessage: message,
+      errorVisible: errorVisible,
+    });
+  },
+
+  handleBlur: function (event) {
+    var valid = this.props.validate(event.target.value);
+    this.validation(event.target.value, valid);
+  },
+  render: function () {
+    return (
+      <div className={this.props.uniqueName}>
+        <input
+          name={this.props.uniqueName}
+          type="date"
+          id={this.props.uniqueName}
+          placeholder={this.props.text}
+          className={"input input-" + this.props.uniqueName}
+          onChange={this.handleChange}
+          onBlur={this.handleBlur}
+          value={this.props.value}
+        />
+
+        <InputError
+          visible={this.state.errorVisible}
+          errorMessage={this.state.errorMessage}
+        />
+      </div>
+    );
+  },
+});
+
+var TimeInput = React.createClass({
+  getInitialState: function () {
+    return {
+      isEmpty: true,
+      value: null,
+      valid: false,
+      errorMessage: "",
+      errorVisible: false,
+    };
+  },
+
+  handleChange: function (event) {
+    this.validation(event.target.value);
+
+    if (this.props.onChange) {
+      this.props.onChange(event);
+    }
+  },
+
+  validation: function (value, valid) {
+    if (typeof valid === "undefined") {
+      valid = true;
+    }
+
+    var message = "";
+    var errorVisible = false;
+
+    if (!valid) {
+      message = this.props.errorMessage;
+      valid = false;
+      errorVisible = true;
+    } else if (this.props.required && jQuery.isEmptyObject(value)) {
+      message = this.props.emptyMessage;
+      valid = false;
+      errorVisible = true;
+    } else if (value.length < this.props.minCharacters) {
+      message = this.props.errorMessage;
+      valid = false;
+      errorVisible = true;
+    }
+
+    this.setState({
+      value: value,
+      isEmpty: jQuery.isEmptyObject(value),
+      valid: valid,
+      errorMessage: message,
+      errorVisible: errorVisible,
+    });
+  },
+
+  handleBlur: function (event) {
+    var valid = this.props.validate(event.target.value);
+    this.validation(event.target.value, valid);
+  },
+  render: function () {
+    return (
+      <div className={this.props.uniqueName}>
+        <input
+          name={this.props.uniqueName}
+          type="time"
+          id={this.props.uniqueName}
+          placeholder={this.props.text}
+          className={"input input-" + this.props.uniqueName}
+          onChange={this.handleChange}
+          onBlur={this.handleBlur}
+          value={this.props.value}
+          step="480"
+          min="10:00"
+          max="18:00"
+        />
+
+        <InputError
+          visible={this.state.errorVisible}
+          errorMessage={this.state.errorMessage}
+        />
+      </div>
+    );
+  },
+});
+
+var PlayerList = React.createClass({
+  render: function () {
+    var optionNodes = this.props.data.map(function (plrID) {
       return (
-        <select name="playernum" id="playernum">
-          <option key="1" value="Player 1">
-            Player 1
-          </option>
-          <option key="2" value="Player 2">
-            Player 2
-          </option>
-          <option key="3" value="Player 3">
-            Player 3
-          </option>
-        </select>
+        <option key={plrID.dbplayer_id} value={plrID.dbplayer_id}>
+          {plrID.dbplayer_firstname} {plrID.dbplayer_lastname}
+        </option>
       );
-    },
-  });
-  
-  var UserList = React.createClass({
-    render: function () {
+    });
+    return (
+      <select name="plrnum" id="plrnum">
+        {optionNodes}
+      </select>
+    );
+  },
+});
+
+var UserList = React.createClass({
+  render: function () {
+    var optionNodes = this.props.data.map(function (usrID) {
       return (
-        <select name="usernum" id="usernum">
-          <option key="1" value="User 1">
-            User 1
-          </option>
-          <option key="2" value="User 2">
-            User 2
-          </option>
-          <option key="3" value="User 3">
-            User 3
-          </option>
-        </select>
+        <option key={usrID.dbuser_id} value={usrID.dbuser_id}>
+          {usrID.dbuser_firstname} {usrID.dbuser_lastname}
+        </option>
       );
-    },
-  });
-  
-  ReactDOM.render(<ReservationBox />, document.getElementById("content"));
-  
+    });
+    return (
+      <select name="usrnum" id="usrnum">
+        {optionNodes}
+      </select>
+    );
+  },
+});
+
+ReactDOM.render(<ReservationBox />, document.getElementById("content"));
--- shelley_milestone3/server.js
+++ shelley_milestone3/server.js
@@ -30,6 +30,13 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
+/* 
+
+  Search Page GET API calls
+
+*/
+
+// Search Products
 app.get("/getproduct/", function (req, res) {
   var pid = req.query.productid;
   var pname = req.query.productname;
@@ -61,7 +68,7 @@ app.get("/getproduct/", function (req, res) {
     res.send(JSON.stringify(data));
   });
 });
-
+// Search Reservations
 app.get("/getreservation/", function (req, res) {
   var rid = req.query.reservationid;
   var rdatetime = req.query.reservationdatetime;
@@ -92,6 +99,70 @@ app.get("/getreservation/", function (req, res) {
       process.exit(1);
     }
     res.send(JSON.stringify(data));
+  });
+});
+
+/* 
+
+  Insert Page GET Data API calls
+
+*/
+
+// Insert Reservations - Player Data
+app.get("/getplrdata/", function (req, res) {
+  var sqlsel =
+    "SELECT * FROM Players ORDER BY dbplayer_lastname, dbplayer_firstname";
+  var sql = mysql.format(sqlsel);
+
+  con.query(sql, function (err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.send(JSON.stringify(data));
+  });
+});
+// Insert Reservations - User Data
+app.get("/getusrdata/", function (req, res) {
+  var sqlsel = "SELECT * FROM Users ORDER BY dbuser_lastname, dbuser_firstname";
+  var sql = mysql.format(sqlsel);
+
+  con.query(sql, function (err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.send(JSON.stringify(data));
+  });
+});
+
+/* 
+
+  Insert Page POST API calls
+
+*/
+
+// Insert Reservations
+app.post("/reservation/", function (req, res) {
+  var rdate = req.body.reservationdate;
+  var rtime = req.body.reservationtime;
+  var pid = req.body.playerid;
+  var uid = req.body.userid;
+
+  var sqlins =
+    "INSERT INTO Reservations (dbreservation_datetime, dbplayer_id, dbuser_id) VALUES (?, ?, ?)";
+  var rdatetime = rdate + " " + rtime;
+  var inserts = [rdatetime, parseInt(pid), parseInt(uid)];
+
+  var sql = mysql.format(sqlins, inserts);
+
+  console.log(sql);
+
+  con.execute(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+    res.redirect("insertreservations.html");
+    res.end();
   });
 });
 
